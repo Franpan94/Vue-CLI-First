abcf7ddd2bd6da67d4fa31390919fe662e5c859d
Franpan94 <francescopanico94@libero.it>
2022-07-14 16:40:38 +0200

    jumbotron creation

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="https://static.turbosquid.com/Preview/2016/02/04__10_52_59/Jumbotron01.jpg8681f0bf-282c-4da3-bfa5-38ad282afe26Large.jpg" alt="jumbotron">
+  </div>
+</template>
+
+<script>
+export default {
+
+    name: 'SuperMain',
+
+}
+</script>
+
+<style>
+
+</style>
